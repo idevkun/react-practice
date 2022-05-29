7edea29935c11b2fb18fcf623e051bdf985b35dc
Aybek Amanbaev <idevkun@gmail.com>
2022-05-29 10:02:59 +0500

    'added images'

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
 import React from "react"
-import Hero from "./components/Hero"
 import Navbar from "./components/Navbar"
-/*
-Challenge: Build the Navbar component.
-Check the Figma file for the design specifics.
-*/
+import Hero from "./components/Hero"
+import Card from "./components/Card"
+import data from "./data"
+
 
 export default function App() {
+    // <Hero />
+    const cards = data.map(item => {
+        return (
+            <Card
+                img={item.coverImg}
+                rating={item.stats.rating}
+                reviewCount={item.stats.reviewCount}
+                location={item.location}
+                title={item.title}
+                price={item.price}
+            />
+        )
+    })
+
+
     return (
         <div>
             <Navbar />
-            <Hero/>
+            {cards}
         </div>
     )
 }
